765e064fac1e0191a56321a83a327fce10a3e8f1
ajoojuice <ajoo@yonsei.ac.kr>
2025-03-26 17:18:42 +0900

    Prevent line wrapping when showing data_df
    style: prevents line wrapping for easy view of data_df
    
    improves readability for wide dataframes.
    
    added width expansion so df_data will show all headers in one line.
    
    currently: last header "gasonlinebob" is entered into the next line when showing df_data

---
+++ code/HE.py
@@ -0,0 +1,15 @@
+# Break the time series into chunks.
+
+# In each chunk:
+
+# Compute the mean and subtract it.
+
+# Compute the cumulative deviation (like a walk).
+
+# Find the range (max - min of cumulative sum).
+
+# Divide by the standard deviation.
+
+# Plot log(R/S) vs log(n) (window size).
+
+# The slope of the line is the Hurst exponent!
